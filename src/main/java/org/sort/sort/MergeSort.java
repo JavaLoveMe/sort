838447aae671ae95259c12b5b5d87b7a080bd3f1
huchengyu <1297670407@qq.com>
2020-08-05 00:44:53 +0800

    完成归并排序

---
+++ src/main/java/org/sort/sort/MergeSort.java
@@ -0,0 +1,44 @@
+package org.sort.sort;
+
+public class MergeSort {
+
+    static void merge_sort_recursive(int[] arr, int[] result, int start, int end) {
+        if (start >= end)
+            return;
+        int len = end - start, mid = (len >> 1) + start;
+        int start1 = start, end1 = mid;
+        int start2 = mid + 1, end2 = end;
+        merge_sort_recursive(arr, result, start1, end1);
+        merge_sort_recursive(arr, result, start2, end2);
+        int k = start;
+        while (start1 <= end1 && start2 <= end2)
+            result[k++] = arr[start1] > arr[start2] ? arr[start1++] : arr[start2++];
+        while (start1 <= end1)
+            result[k++] = arr[start1++];
+        while (start2 <= end2)
+            result[k++] = arr[start2++];
+        for (k = start; k <= end; k++)
+            arr[k] = result[k];
+        output(result);
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[]{17, 11, 4, 12, 10, 6, 15, 5, 3, 8};
+        int len = array.length;
+        int[] result = new int[len];
+        merge_sort_recursive(array, result, 0, len - 1);
+    }
+
+    private static void output(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (i == 0) {
+                System.out.print("[" + array[0] + ", ");
+            } else if (i == array.length - 1) {
+                System.out.println(array[i] + "]");
+            } else {
+                System.out.print(array[i] + ", ");
+            }
+
+        }
+    }
+}
